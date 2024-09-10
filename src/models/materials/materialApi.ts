92a347b4e0a40c5dd5985cfad1f58ede7455fc73
Ilyas Safiullin <Safiullin1Ilyas@yandex.ru>
2024-09-10 23:51:19 +0300

    Material model

---
+++ src/models/materials/materialApi.ts
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { rootReducer } from "@utils/reducer.ts";
+import { materialSchema } from "@models/materials/schema.ts";
+
+const baseUrl = import.meta.env.VITE_BASE_URL;
+
+export const materialApi = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  reducerPath: "materialApi",
+  tagTypes: ["Materials"],
+  endpoints: (builder) => ({
+    getMaterials: builder.query<any, void>({
+      query: () => `${baseUrl}/materials`,
+      transformResponse: (response) => {
+        return materialSchema.parse(response);
+      },
+    }),
+  }),
+});
+
+rootReducer.inject(materialApi);
