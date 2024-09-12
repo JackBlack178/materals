c9499a410bac742f5a9628f19a2bf9eeb073ea6d
Ilyas Safiullin <Safiullin1Ilyas@yandex.ru>
2024-09-12 11:43:56 +0300

    postcss autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import * as path from "node:path";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer({})],
+    },
+  },
   base: "/materials/",
   server: {
     strictPort: true,
